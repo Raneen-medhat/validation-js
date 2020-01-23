d93f9804a701269b024f31d9c72971a22f694650
Raneen <raneen.medhat@gmail.com>
2020-01-23 14:21:19 +0200

    update email pattern

--- patterns.js
+++ patterns.js
@@ -1,6 +1,6 @@
 export  const character =  /^[a-zA-Z]+$/;
 export  const number    =  /^[0-9]+$/;
-export  const email     = /^[\w]+(?:[-.\w]+)*@+(?:[.a-z0-9])+$/;
+export  const email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})$/
 export  const arNumbers = ['٠', '١' , '٢','٣' ,'٤', '٥', '٦', '٧', '٨','٩'];
 export  const mobile    = /^(009665|9665|\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/;
 export  const arNumber  = /^[\u0621-\u064A]$/;
